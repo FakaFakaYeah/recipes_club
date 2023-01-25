539a26771c0dfbda15ca08743088fc1ae9389a3c
ÐСтепнан СмирновÑÑ <fakafaka270695@yandex.ru>
2023-01-25 19:27:22 +0300

    Добавил модель тегов и ингредиентов

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -145,6 +145,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
+    'DEFAULT_FILTER_BACKENDS':
+        'django_filters.rest_framework.DjangoFilterBackend',
+    'SEARCH_PARAM': 'name',
 }
 
 DJOSER = {
