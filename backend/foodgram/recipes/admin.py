2a097b7145ae8be80da90ec5a36da285d01ac63e
ÐСтепнан СмирновÑÑ <fakafaka270695@yandex.ru>
2023-01-26 13:47:55 +0300

    Добавил модель тегов и ингредиентов

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-from .models import Ingredients, Tag
+from .models import Ingredients, Tag, Recipes, IngredientsInRecipe
 
 
 @admin.register(Ingredients)
@@ -14,3 +15,31 @@ class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'color', 'slug')
     search_fields = ('name',)
 
+
+class IngredientInRecipeInline(admin.TabularInline):
+    model = IngredientsInRecipe
+
+
+@admin.register(Recipes)
+class Recipes(admin.ModelAdmin):
+    inlines = [
+        IngredientInRecipeInline,
+    ]
+    list_display = (
+        'name', 'author', 'image', 'text', 'cooking_time', 'pub_date'
+    )
+    filter_horizontal = ('tags',)
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, image_preview):
+        return format_html(
+            '<img src="/media/{}" height=200px width=auto />',
+            image_preview.image
+        )
+
+    image_preview.short_description = 'Фото рецепта'
+
+
+@admin.register(IngredientsInRecipe)
+class IngredientsInRecipeAdmin(admin.ModelAdmin):
+    list_display = ('recipes', 'ingredient', 'amount')
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,5 +1,12 @@
 from colorfield.fields import ColorField
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+
+from users.models import User
+
+MIN_COOK_TIME = 1
+MAX_COOK_TIME = 10080
+MIN_AMOUNT = 1
 
 
 class Ingredients(models.Model):
@@ -14,6 +21,9 @@ class Ingredients(models.Model):
 
     class Meta:
         verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
@@ -34,4 +44,78 @@ class Tag(models.Model):
     )
 
     class Meta:
-        verbose_name_plural = 'Тэги'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+
+class Recipes(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='recipes',
+        verbose_name='Автор'
+    )
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    image = models.ImageField(
+        upload_to='recipes/images',
+        verbose_name='Фото'
+    )
+    text = models.TextField(
+        verbose_name='Описание',
+        help_text='Введите описание вашего рецепта'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Тег'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredients,
+        through='IngredientsInRecipe',
+        verbose_name='Ингредиент'
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(MIN_COOK_TIME, 'Нельзя готовить ноль минут!'),
+            MaxValueValidator(MAX_COOK_TIME, 'Нельзя готовить блюдо неделю!')
+        ],
+        verbose_name='Время приготовления',
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата добавления рецепта'
+    )
+
+    class Meta:
+        verbose_name_plural = 'Рецепты'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientsInRecipe(models.Model):
+    recipes = models.ForeignKey(
+        Recipes,
+        on_delete=models.CASCADE
+    )
+    ingredient = models.ForeignKey(
+        Ingredients,
+        on_delete=models.CASCADE
+    )
+    amount = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(MIN_AMOUNT, 'Неправильная величина продукта!')
+        ]
+    )
+
+    class Meta:
+        verbose_name_plural = 'Рецепты + ингредиенты'
+
+    def __str__(self):
+        return f'{self.ingredient} --> {self.recipes}'
