dc2bd5f547e25f5eb3a71869823965f749f1475a
ÐСтепнан СмирновÑÑ <fakafaka270695@yandex.ru>
2023-02-09 00:21:26 +0300

    Правки после ревью

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,7 +1,7 @@
 from django_filters.rest_framework import FilterSet, filters
 from rest_framework.filters import SearchFilter
 
-from recipes.models import Recipe, Tag, ShoppingCart
+from recipes.models import Recipe, Tag
 
 
 class RecipeFilter(FilterSet):
