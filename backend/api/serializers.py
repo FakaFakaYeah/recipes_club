d92205988f3f7c8db3e2194d3b50115c58a011dc
ÐСтепнан СмирновÑÑ <fakafaka270695@yandex.ru>
2023-02-21 21:42:06 +0300

    Правки после ревью

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -103,7 +103,7 @@ class RecipeCreateSerializer(RecipeReadSerializer):
 
     @staticmethod
     def validate_image(value):
-        if value.size > 0.1 * 1024 * 1024:
+        if value.size > 1 * 1024 * 1024:
             raise serializers.ValidationError(
                 'Картинки размером больше 1Mb не поддерживаются'
             )
--- backend/recipes/validators.py
+++ backend/recipes/validators.py
@@ -2,6 +2,6 @@ from django.core.exceptions import ValidationError
 
 
 def image_size(value):
-    if value.size > 0.1 * 1024 * 1024:
+    if value.size > 1 * 1024 * 1024:
         raise ValidationError('Слишком большое фото!')
     return value
