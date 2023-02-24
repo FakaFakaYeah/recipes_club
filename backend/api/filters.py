c851616c8b07aae134858a2f8de465cc64c46f08
ÐСтепнан СмирновÑÑ <fakafaka270695@yandex.ru>
2023-02-24 20:21:30 +0300

    Правки после ревью

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,19 +1,19 @@
-from django_filters.rest_framework import FilterSet, filters
+from django_filters import rest_framework
 from rest_framework.filters import SearchFilter
 
 from recipes.models import Recipe, Tag
 
 
-class RecipeFilter(FilterSet):
+class RecipeFilter(rest_framework.FilterSet):
     """Фильтр для рецептов. Поиск slug тэга,
     любимым рецептам и рецептам в списке покупок."""
 
-    tags = filters.ModelMultipleChoiceFilter(
+    tags = rest_framework.ModelMultipleChoiceFilter(
         field_name="tags__slug",
         queryset=Tag.objects.all(),
         to_field_name="slug", )
-    is_favorited = filters.BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_favorited = rest_framework.BooleanFilter(method='get_is_favorited')
+    is_in_shopping_cart = rest_framework.BooleanFilter(
         method='get_is_in_shopping_cart')
 
     class Meta:
